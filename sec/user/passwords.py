79c67b74d0262de85776b5212cf9221680f45b73
kotlarz <marius.kotlarz@gmail.com>
2019-02-15 13:31:37 +0100

    Add two example FIXME's

--- sec/user/passwords.py
+++ sec/user/passwords.py
@@ -4,4 +4,5 @@ from django.contrib.auth.hashers import MD5PasswordHasher
 class CustomMD5PasswordHasher(MD5PasswordHasher):
 
     def salt(self):
+        # FIXME: Static salt (OTG-CRYPST-004?)
         return "tdt4237"
