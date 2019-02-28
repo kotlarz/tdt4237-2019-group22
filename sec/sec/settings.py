1623466bfe4e6595989a6a66e59126e285b7c2bb
Lise Hove <lisehove@hotmail.com>
2019-02-28 19:00:42 +0100

    fix/A2 Add initial password verification

--- sec/sec/settings.py
+++ sec/sec/settings.py
@@ -104,7 +104,20 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
 # FIXME: Insecure Password Hashing - Static salt (OTG-CRYPST-004?)
 # TODO: https://docs.djangoproject.com/en/2.1/topics/auth/passwords/
