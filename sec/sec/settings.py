c4b46ad501276e376775759074c17d38d9c55092
Bjørn Rude Jacobsen <bjornrja@stud.ntnu.no>
2019-03-12 18:50:55 +0100

    Revert "Fixed some cookie attributes, set debug to False, removed 0.0.0.0 as allowed host"
    Pushed to the wrong branch...
    
    This reverts commit ca7362ba7e5b65e81418a4561056937f850ebf44.

--- sec/sec/settings.py
+++ sec/sec/settings.py
@@ -25,16 +25,26 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # FIXME: Replace in production
 SECRET_KEY = '$n%^#g%qx#82w6t^dvjqwv)q*1cy+fwh1ohku7-rbjqcei2^jr'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+# FIXME: Security Misconfiguration - Set to False in production
+"""
+The application is running in debug mode, meaning that an attacker
+can gain valuable information from the stack traces shown when
+an internal server error occurs.
+Update: The cookie attributes are configured insecured,
+making it easier for an attacker to steal the cookie.
+"""
+DEBUG = True
 
-DEBUG = False
-
-#TODO
-""""Set path and domain"""
-#SESSION_COOKIE_PATH='';
-#SESSION_COOKIE_DOMAIN='';
-
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# FIXME: Allowed hosts
+"""
+Lists of allowed_hosts is intended to be a white-list and
+including 0.0.0.0 means that any host is accepted.
+Accepting any host is not recommend as this gives attackers the
+opportunity to make Django generate and display URL’s to users
+on arbitrary domains.
+"""
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 # Application definition
 
