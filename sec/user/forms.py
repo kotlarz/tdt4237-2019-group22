a1492f74a0ca077322b289416102094511fd1058
kotlarz <marius.kotlarz@gmail.com>
2019-03-14 20:24:53 +0100

    Add security question models, add it to the signup form and save

--- sec/user/forms.py
+++ sec/user/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.forms.utils import ErrorList
+
 from projects.models import ProjectCategory
+from user.models import SecurityQuestion
+
+
+class SecurityQuestionChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.question
+
+
+SECURITY_UNIQUE_VALIDATION_ERROR_MESSAGE = "Security questions need to be unique"
 
 
 class SignUpForm(UserCreationForm):
@@ -19,10 +31,45 @@ class SignUpForm(UserCreationForm):
                                                 help_text='Hold down "Control", or "Command" on a Mac, to select more than one.',
                                                 required=False)
 
+    security_question_1 = SecurityQuestionChoiceField(queryset=SecurityQuestion.objects.all(),
+                                                      help_text='Select your first Security Question')
+    security_question_1_answer = forms.CharField(max_length=250)
+
+    security_question_2 = SecurityQuestionChoiceField(queryset=SecurityQuestion.objects.all(),
+                                                      help_text='Select your second Security Question')
+    security_question_2_answer = forms.CharField(max_length=250)
+
+    security_question_3 = SecurityQuestionChoiceField(queryset=SecurityQuestion.objects.all(),
+                                                      help_text='Select your third Security Question')
+    security_question_3_answer = forms.CharField(max_length=250)
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'categories', 'company', 'email', 'password1', 'password2',
-                  'phone_number', 'street_address', 'city', 'state', 'postal_code', 'country')
+        fields = ('username', 'first_name', 'last_name', 'categories', 'company', 'email',
+                  'password1', 'password2', 'security_question_1', 'security_question_1_answer',
+                  'security_question_2', 'security_question_2_answer', 'security_question_3',
+                  'security_question_3_answer', 'phone_number', 'street_address', 'city', 'state',
+                  'postal_code', 'country')
+
+    def clean_security_question_2(self):
+        security_question_1 = self.cleaned_data.get("security_question_1")
+        security_question_2 = self.cleaned_data.get("security_question_2")
+        security_question_3 = self.cleaned_data.get("security_question_3")
+
+        if security_question_2 in [security_question_1, security_question_3]:
+            raise ValidationError(SECURITY_UNIQUE_VALIDATION_ERROR_MESSAGE)
+
+        return security_question_2
+
+    def clean_security_question_3(self):
+        security_question_1 = self.cleaned_data.get("security_question_1")
+        security_question_2 = self.cleaned_data.get("security_question_2")
+        security_question_3 = self.cleaned_data.get("security_question_3")
+
+        if security_question_3 in [security_question_1, security_question_2]:
+            raise ValidationError(SECURITY_UNIQUE_VALIDATION_ERROR_MESSAGE)
+
+        return security_question_3
 
 
 class LoginForm(forms.Form):
